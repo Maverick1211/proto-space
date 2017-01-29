1764f9cabe947bf3968295dacbf3f2c017dc87a5
Maverick1211 <tensho1211@icloud.com>
2017-01-30 08:24:19 +0900

    change username to name

--- db/migrate/20170129054051_add_few_columns_to_users.rb
+++ db/migrate/20170129054051_add_few_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddFewColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :username, :string, null: false
-    add_index :users, :username
+    add_column :users, :name, :string, null: false
+    add_index :users, :name
     add_column :users, :avatar, :string
     add_column :users, :profile, :text
     add_column :users, :position, :string
