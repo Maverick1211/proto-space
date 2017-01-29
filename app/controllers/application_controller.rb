32bbdecc4e58816b2f07b0e3a36e634a4e2341b0
Maverick1211 <tensho1211@icloud.com>
2017-01-29 19:50:04 +0900

    sign_in and sign_out function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation, :username, :avatar, :profile, :position, :occupation)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password, :remember_me)
+    end
+  end
 end
