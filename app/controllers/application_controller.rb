383c233aec63744caef395413e3d339d5ce31076
Maverick1211 <tensho1211@icloud.com>
2017-01-30 08:16:23 +0900

    remove authenticate_user and edit permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:email, :password, :password_confirmation, :username, :avatar, :profile, :position, :occupation)
-    end
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:email, :password, :remember_me)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar, :profile, :position, :occupation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:remember_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :profile, :position, :occupation])
   end
 end
