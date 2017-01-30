b37eda87e1db89bb3cd8fd62d295776c088e4808
Maverick1211 <tensho1211@icloud.com>
2017-01-30 21:35:34 +0900

    add function of editting user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to 'http://localhost:3000/'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile, :position, :occupation)
+  end
 end
