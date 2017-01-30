982fa1108782116ef55970f44e67294cb2551767
Maverick1211 <tensho1211@icloud.com>
2017-01-31 07:38:14 +0900

    modify few points

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to root_path
+    if @user == current_user
+      @user.update(user_params)
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :profile, :position, :occupation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
