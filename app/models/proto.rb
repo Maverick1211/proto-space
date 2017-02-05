ca50f8fe68e03507a1aa8911edcc4b33aa598afa
Maverick1211 <tensho1211@icloud.com>
2017-02-06 02:23:02 +0900

    modify association name

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -1,8 +1,9 @@
 class Proto < ApplicationRecord
   belongs_to :user
   has_many :images
-  has_one :image, -> { where(role: :main) }
+  has_one :main_image, -> { where role: 'main' }, class_name: 'Image'
   accepts_nested_attributes_for :images, allow_destroy: true
-
   validates :title, :catchcopy, :concept, presence: true
+
+  SUB_COUNT= 3
 end
