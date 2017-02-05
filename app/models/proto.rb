eff02bed27c2d84932c9b756a17ceb5c8668e3cb
Maverick1211 <tensho1211@icloud.com>
2017-02-06 00:34:44 +0900

    modify roles

--- app/models/proto.rb
+++ app/models/proto.rb
@@ -1,7 +1,7 @@
 class Proto < ApplicationRecord
   belongs_to :user
   has_many :images
-  has_one :image, -> { where(role: "main") }
+  has_one :image, -> { where(role: :main) }
   accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :title, :catchcopy, :concept, presence: true
