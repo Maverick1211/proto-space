219fc2e67193452dc2baa43bff4aaa393a44cc48
Maverick1211 <tensho1211@icloud.com>
2017-02-05 20:49:21 +0900

    modify few points

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'protos#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :protos
-  root to: 'protos#index'
 end
