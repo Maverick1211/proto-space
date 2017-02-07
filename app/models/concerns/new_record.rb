b9a88cea78146f06fba252c6fb30420028c57b09
Maverick1211 <tensho1211@icloud.com>
2017-02-07 22:52:32 +0900

    create module about initializing

---
+++ app/models/concerns/new_record.rb
@@ -0,0 +1,30 @@
+module NewRecord
+  extend ActiveSupport::Concern
+  included do
+    def new_record?(strict = false)
+      if strict
+        self.class.validated_attributes_on(:presence).map { |attr|
+         self.send(attr).nil?
+        }.exclude?(false)
+      else
+        super()
+      end
+    end
+
+    private
+
+    def self.validated_attributes_on(type)
+      key =  "#{type.to_s.camelize}Validator"
+
+      begin
+        validator = const_get(key)
+      rescue NameError
+        raise ArgumentError, "Unknown validator: #{key}"
+      end
+
+      _validators.map do |attr, validators|
+        attr if validators.map(&:class).include?(validator)
+      end
+    end
+  end
+end
--- app/models/proto.rb
+++ app/models/proto.rb
@@ -5,61 +5,16 @@ class Proto < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :title, :catchcopy, :concept, presence: true
+
+  include NewRecord
+
   after_initialize :build_main_image, :build_sub_images, if: -> { new_record?(true) }
 
   def sub_images
     images.select(&:sub?)
   end
 
-  def new_record?(strict = false) #defaultだとfalseなので、trueを渡して裏で定義されているのを呼ばれないようにする
-    if strict
-      id.nil? && !self.class.validated_attributes_on(:presence).map { |attr|
-      # self.class.validated_attributes_on(:presence) => [ :user, :title, :catchcopy, :concept]
-        self.send(attr).nil?
-        # sendメソッドは、レシーバの持っているメソッドを呼び出す
-      }.include?(false)
-    else
-      super() #defaultの裏で定義されているnew_record?を呼ぶ
-    end
-  end
-
   private
-
-  def self.validated_attributes_on(type) #typeにはvalidationの種類が渡される
-    validator = case type
-                when :absence    then ActiveRecord::Validations::AbsenceValidator
-                when :associated then ActiveRecord::Validations::AssociatedValidator
-                when :length     then ActiveRecord::Validations::LengthValidator
-                when :presence   then ActiveRecord::Validations::PresenceValidator #今回の場合は←が渡される
-                when :uniqueness then ActiveRecord::Validations::UniquenessValidator
-                else raise
-                end
-
-    _validators.map do |attr, validators|
-      attr if validators.map(&:class).include?(validator) #=ActiveRecord::Validations::PresenceValidator
-    end
-    # _validatorsの中身
-       # {:user=>
-       #  [#<ActiveRecord::Validations::PresenceValidator:0x007fe4d39c73d0
-       #    @attributes=[:user],
-       #    @options={:message=>:required}>], validatorsに入る
-       # :title=>
-       #  [#<ActiveRecord::Validations::PresenceValidator:0x007fe4d0c77fb0
-       #    @attributes=[:title, :catchcopy, :concept],
-       #    @options={}>],
-       # :catchcopy=>
-       #  [#<ActiveRecord::Validations::PresenceValidator:0x007fe4d0c77fb0
-       #    @attributes=[:title, :catchcopy, :concept],
-       #    @options={}>],
-       # :concept=>
-       #  [#<ActiveRecord::Validations::PresenceValidator:0x007fe4d0c77fb0
-       #    @attributes=[:title, :catchcopy, :concept],
-       #    @options={}>]}
-#    validators.map(&:class) => [ActiveRecord::Validations::PresenceValidator]
-#    validator => ActiveRecord::Validations::PresenceValidator
-#    attr => :user
-#   _validators.map do |attr, validators| => [ :user, :title, :catchcopy, :concept]
-  end
 
   def build_main_image
     images << Image.new(role: :main)
