a23cf346a73b4e51ddfb1a6589f5a3c12b6e3f9e
Maverick1211 <tensho1211@icloud.com>
2017-02-22 01:10:48 +0900

    Change routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'protos#index'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
+  scope module: :protos do
+    resources :popular, only: :index
+  end
   resources :protos do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
